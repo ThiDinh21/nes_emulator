0b15c72cda4f620d87ee1c7878162001fb97cfa8
Thi Dinh <vietthidinh2001@gmail.com>
2023-01-04 01:05:02 +0200

    test: for LDA

--- src/main.rs
+++ src/main.rs
@@ -74,3 +74,28 @@ impl CPU {
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0xa9_lda_immidiate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        // zero flag should be 0
+        assert!(cpu.status & 0b0000_0010 == 0);
+        // negative flag should be 0
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x0, 0x00]);
+        assert_eq!(cpu.register_a, 0x00);
+        // zero flag should be 1
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+}
