c540908ee21f78adfeaf9e7ca20b7a2dc5ab7f8c
Thi Dinh <vietthidinh2001@gmail.com>
2023-01-04 00:35:38 +0200

    add: 6502 CPU implementation sketch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+pub struct CPU {
+    /// https://www.nesdev.org/wiki/CPU_registers
+    /// reg x, y, a: 1 byte
+    /// status register: 1 byte
+    /// stack pointer: 1 byte
+    /// program counter: 2 bytes
+    pub register_a: u8,
+    pub status: u8,
+    pub program_counter: u16,
+}
+
+impl CPU {
+    pub fn mew() -> Self {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        todo!("implement")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
